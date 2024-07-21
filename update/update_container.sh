c70144bb22921798714bf67e1baca5f942938356
gibbyb <gib@gibbyb.com>
2024-07-20 22:40:16 -0500

    Finally a production ready application

--- update/update_container.sh
+++
@@ -1,4 +0,0 @@
-wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
-source ~/.bashrc
-pnpm build 
-pnpm start
--- update/update_host.sh
+++
@@ -1,4 +0,0 @@
-sudo docker restart techtracker
-cd ~/Documents/Web/Tech_Tracker_Web
-git pull
-sudo docker exec techtracker bash update/update_container.sh
