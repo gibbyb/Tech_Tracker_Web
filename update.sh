e5b5722c5dfddf0359290f7b840369d6bb7e2fea
gibbyb <gib@gibbyb.com>
2024-07-20 21:05:12 -0500

    idek

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 sudo docker restart techtracker
 cd ~/Documents/Web/Tech_Tracker_Web
 git pull
+#sudo docker exec techtracker wget -qO- https://get.pnpm.io/install.sh\
+  #| ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
 sudo docker exec techtracker /home/node/.local/share/pnpm/pnpm build 
 sudo docker exec techtracker /home/node/.local/share/pnpm/pnpm start &
