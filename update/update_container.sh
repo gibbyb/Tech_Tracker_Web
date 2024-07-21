0d8fdf61e644c1a2ff3b467013633652efe20d41
gibbyb <gib@gibbyb.com>
2024-07-20 22:35:47 -0500

    add cmd for custom image

--- update/update_container.sh
+++ update/update_container.sh
@@ -1,4 +1,4 @@
-#wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
-#source ~/.bashrc
-/home/node/.local/share/pnpm/pnpm build 
-/home/node/.local/share/pnpm/pnpm start &
+wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
+source ~/.bashrc
+pnpm build 
+pnpm start
