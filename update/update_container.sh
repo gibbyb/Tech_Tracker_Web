3b2d56b8a529f7879da95e61e6f297fac31242d5
gibbyb <gib@gibbyb.com>
2024-07-20 22:03:58 -0500

    More updates

--- update/update_container.sh
+++ update/update_container.sh
@@ -1,3 +1,4 @@
-wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
+#wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
+#source ~/.bashrc
 /home/node/.local/share/pnpm/pnpm build 
 /home/node/.local/share/pnpm/pnpm start &
--- update/update_host.sh
+++ update/update_host.sh
@@ -1,4 +1,4 @@
 sudo docker restart techtracker
 cd ~/Documents/Web/Tech_Tracker_Web
 git pull
-sudo docker exec techtracker /home/node/app/update/update_container.sh
+sudo docker exec techtracker bash update/update_container.sh
