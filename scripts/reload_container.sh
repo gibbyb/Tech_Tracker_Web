e2e6e4a70ab3d9690ec28c787d462d63bb623437
gibbyb <gib@gibbyb.com>
2025-04-03 10:24:23 -0500

    Update path in script

--- scripts/reload_container.sh
+++ scripts/reload_container.sh
@@ -1,7 +1,7 @@
 git pull
-mv ~/Documents/Web/Tech_Tracker_Web/next.config.js ~/Documents/Web/Tech_Tracker_Web/scripts/next.config.default.js
-cp ~/Documents/Web/Tech_Tracker_Web/scripts/next.config.build.js ~/Documents/Web/Tech_Tracker_Web/next.config.js
+mv ./next.config.js ./scripts/next.config.default.js
+cp ./scripts/next.config.build.js ./next.config.js
 sudo docker compose -f docker/development/compose.yaml down
 sudo docker compose -f docker/development/compose.yaml build
 sudo docker compose -f docker/development/compose.yaml up -d
-cp ~/Documents/Web/Tech_Tracker_Web/scripts/next.config.default.js ~/Documents/Web/Tech_Tracker_Web/next.config.js
+cp ./scripts/next.config.default.js ./next.config.js
